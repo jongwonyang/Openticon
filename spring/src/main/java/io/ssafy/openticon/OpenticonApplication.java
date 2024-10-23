ff18c55a084a613fbd064cb68db3d572433774a5
Roista57 <roista57@gmail.com>
2024-10-23 16:48:05 +0900

    [backend/spring] 백엔드 프로젝트 생성

---
+++ spring/src/main/java/io/ssafy/openticon/OpenticonApplication.java
@@ -0,0 +1,13 @@
+package io.ssafy.openticon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenticonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenticonApplication.class, args);
+	}
+
+}
