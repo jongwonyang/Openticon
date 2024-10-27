3459bf8935ba255291b4d4921446637e1036bb86
roista57 <roista57@gmail.com>
2024-10-27 21:50:02 +0900

    [backend/spring] 포인트 request dto 추가

--- backend/spring/src/main/java/io/ssafy/openticon/controller/PointController.java
+++ backend/spring/src/main/java/io/ssafy/openticon/controller/PointController.java
@@ -3,6 +3,7 @@ package io.ssafy.openticon.controller;
 
 import com.siot.IamportRestClient.IamportClient;
 import com.siot.IamportRestClient.response.Payment;
+import io.ssafy.openticon.dto.PointRequestDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,15 +13,15 @@ import java.util.Map;
 @RestController
 @RequestMapping("/points")
 public class PointController {
-
     @Autowired
     private IamportClient iamportClient;
 
     @PostMapping("/payment")
-    public Map<String, String> paymentComplete(@RequestBody Map<String, String> request) {
+    public Map<String, String> paymentComplete(@RequestBody PointRequestDto request) {
         Map<String, String> response = new HashMap<>();
         // 유저가 건내준 imp_uid을 가져옴 ex)imp_749635900582
-        String impUid = request.get("imp_uid");
+        String impUid = request.getImpUid();
+        System.out.println(request.getMemberId()+"("+request.getImpUid()+"): "+request.getEmoticonPackId()+", "+request.getPoint());
 
         try {
             // iamport 서버에서 유저의 imp_uid 값을 확인해서 결제 내역이 있는지 확인 >> 결제가 성공이라면 paid
@@ -36,10 +37,8 @@ public class PointController {
             }
         } catch (Exception e) { // 결제 내역이 없는 경우
             System.out.println("error");
-//            e.printStackTrace();
             response.put("status", "error");
         }
-
         return response;
     }
 }
