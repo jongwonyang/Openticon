5cab4ecb2b7f2f2a8601d8e0d5102e037505e1ad
roista57 <roista57@gmail.com>
2024-10-28 22:40:17 +0900

    [backend/spring] 포인트 컨트롤러 및 서비스 수정, PointHistoryRepository 생성

--- backend/spring/src/main/java/io/ssafy/openticon/controller/PointController.java
+++ backend/spring/src/main/java/io/ssafy/openticon/controller/PointController.java
@@ -6,6 +6,7 @@ import com.siot.IamportRestClient.response.Payment;
 import io.ssafy.openticon.controller.request.PointRequestDto;
 import io.ssafy.openticon.entity.Member;
 import io.ssafy.openticon.repository.MemberRepository;
+import io.ssafy.openticon.service.PointService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,39 +24,52 @@ import java.util.Map;
 @Tag(name = "포인트")
 public class PointController {
     @Autowired
-    private IamportClient iamportClient;
+    private MemberRepository memberRepository;
 
     @Autowired
-    private MemberRepository memberRepository;
+    private PointService pointService;
 
     @PostMapping("/payment")
     @Operation(summary = "사용자의 결제에 대한 검증을 합니다.")
-    public Map<String, String> paymentComplete(@RequestBody PointRequestDto request, @AuthenticationPrincipal UserDetails userDetails) {
+    public String paymentComplete(@RequestBody PointRequestDto request, @AuthenticationPrincipal UserDetails userDetails) {
         // 유저 정보 가지고옴
         Member member = memberRepository.findMemberByEmail(userDetails.getUsername())
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "사용자가 없습니다."));
-
-        Map<String, String> response = new HashMap<>();
-        // 유저가 건내준 imp_uid을 가져옴 ex)imp_749635900582
-        String impUid = request.getImpUid();
-        System.out.println(member.getId()+"("+request.getImpUid()+"): "+request.getEmoticonPackId()+", "+request.getPoint());
-
-        try {
-            // iamport 서버에서 유저의 imp_uid 값을 확인해서 결제 내역이 있는지 확인 >> 결제가 성공이라면 paid
-            Payment payment = iamportClient.paymentByImpUid(impUid).getResponse();
-
-            // 아래는 결제 내역이 있는 경우
-            if ("paid".equals(payment.getStatus())) {
-                System.out.println(payment.getStatus());
-                response.put("status", "success");
-            } else {
-                System.out.println(payment.getStatus());
-                response.put("status", "fail");
-            }
-        } catch (Exception e) { // 결제 내역이 없는 경우
-            System.out.println("error");
-            response.put("status", "error");
-        }
-        return response;
+        return pointService.paymentCheck(request.getImpUid()); // success, fail, error
     }
+
+    @PostMapping("/purchase-point")
+    @Operation(summary = "사용자가 포인트를 구매합니다.")
+    public String purchasePoint(@RequestBody PointRequestDto request, @AuthenticationPrincipal UserDetails userDetails) {
+        // 유저 정보 가지고옴
+        Member member = memberRepository.findMemberByEmail(userDetails.getUsername())
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "사용자가 없습니다."));
+        if(pointService.purchasePoints(request, member)){
+            return "success";
+        }
+        return "fail";
+    }
+
+    @PostMapping("/purchase-pack")
+    @Operation(summary = "사용자가 이모티콘 팩을 구매합니다.")
+    public String purchasePack(@RequestBody PointRequestDto request, @AuthenticationPrincipal UserDetails userDetails) {
+
+        return "success";
+    }
+
+    @GetMapping("/list")
+    @Operation(summary = "사용자가 이모티콘 팩을 구매합니다.")
+    public String getPointsList(@RequestBody PointRequestDto request, @AuthenticationPrincipal UserDetails userDetails) {
+
+        return "success";
+    }
+
+    @PostMapping("/withdraw-point")
+    @Operation(summary = "사용자가 이모티콘 팩을 구매합니다.")
+    public String withdrawPoint(@RequestBody PointRequestDto request, @AuthenticationPrincipal UserDetails userDetails) {
+
+        return "success";
+    }
+
+
 }
