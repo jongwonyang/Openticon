8eba4ad5ec4af93c97495541cc49a2f5f8123932
roista57 <roista57@gmail.com>
2024-10-28 17:39:38 +0900

    [backend/spring] 포인트 컨트롤러 및 request dto 수정 포인트 컨트롤러에서 멤버 정보를 헤더에서 가져오도록 수정 request dto 에서 멤버 아이디를 받는 부분을 삭제

--- backend/spring/src/main/java/io/ssafy/openticon/controller/PointController.java
+++ backend/spring/src/main/java/io/ssafy/openticon/controller/PointController.java
@@ -4,24 +4,41 @@ package io.ssafy.openticon.controller;
 import com.siot.IamportRestClient.IamportClient;
 import com.siot.IamportRestClient.response.Payment;
 import io.ssafy.openticon.controller.request.PointRequestDto;
+import io.ssafy.openticon.entity.Member;
+import io.ssafy.openticon.repository.MemberRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/points")
+@Tag(name = "포인트")
 public class PointController {
     @Autowired
     private IamportClient iamportClient;
 
+    @Autowired
+    private MemberRepository memberRepository;
+
     @PostMapping("/payment")
-    public Map<String, String> paymentComplete(@RequestBody PointRequestDto request) {
+    @Operation(summary = "사용자의 결제에 대한 검증을 합니다.")
+    public Map<String, String> paymentComplete(@RequestBody PointRequestDto request, @AuthenticationPrincipal UserDetails userDetails) {
+        // 유저 정보 가지고옴
+        Member member = memberRepository.findMemberByEmail(userDetails.getUsername())
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "사용자가 없습니다."));
+
         Map<String, String> response = new HashMap<>();
         // 유저가 건내준 imp_uid을 가져옴 ex)imp_749635900582
         String impUid = request.getImpUid();
-        System.out.println(request.getMemberId()+"("+request.getImpUid()+"): "+request.getEmoticonPackId()+", "+request.getPoint());
+        System.out.println(member.getId()+"("+request.getImpUid()+"): "+request.getEmoticonPackId()+", "+request.getPoint());
 
         try {
             // iamport 서버에서 유저의 imp_uid 값을 확인해서 결제 내역이 있는지 확인 >> 결제가 성공이라면 paid
