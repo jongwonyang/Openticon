78b7a3bf18b136edeecfeabc476fdc607b995d93
박병준 <h0568@naver.com>
2024-10-29 13:16:18 +0900

    [android/floating] Delete viewmodel from OpenticonApplication.kt

--- android/app/src/main/java/io/ssafy/openticon/OpenticonApplication.kt
+++ android/app/src/main/java/io/ssafy/openticon/OpenticonApplication.kt
@@ -2,7 +2,6 @@ package io.ssafy.openticon
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
-import io.ssafy.openticon.ui.viewmodel.ImoticonViewModel
 
 @HiltAndroidApp
 class OpenticonApplication : Application() {
