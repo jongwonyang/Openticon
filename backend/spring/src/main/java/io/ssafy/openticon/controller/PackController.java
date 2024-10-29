a4eedcd7ebea4290755e88ff6f13191be757035a
강도원 <3120kang@naver.com>
2024-10-29 09:12:02 +0900

    [backend/spring] 이모티콘팩 업로드 서비스 추가

--- backend/spring/src/main/java/io/ssafy/openticon/controller/PackController.java
+++ backend/spring/src/main/java/io/ssafy/openticon/controller/PackController.java
@@ -1,9 +1,11 @@
 package io.ssafy.openticon.controller;
 
 import io.ssafy.openticon.controller.request.EmoticonUploadRequestDto;
+import io.ssafy.openticon.dto.EmoticonPack;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -13,12 +15,16 @@ import java.util.List;
 @RestController
 public class PackController {
 
+    @PostMapping("/upload")
     public ResponseEntity<Void> uploadEmoticon(@AuthenticationPrincipal UserDetails userDetails,
                                                @RequestPart("packInfo") EmoticonUploadRequestDto emoticonUploadRequest,
-                                               @RequestPart("thumbnail")MultipartFile thumbnail,
-                                               @RequestPart("thumbnail_list") MultipartFile thumbnailList,
+                                               @RequestPart("thumbnail_img")MultipartFile thumbnailImg,
+                                               @RequestPart("list_img") MultipartFile listImg,
                                                @RequestPart("emoticons")List<MultipartFile> emoticons
                                                ){
+
+        EmoticonPack emoticonPack=new EmoticonPack(emoticonUploadRequest);
+        emoticonPack.setImages(thumbnailImg, listImg, emoticons);
         return ResponseEntity.noContent().build();
     }
 }
--- backend/spring/src/main/java/io/ssafy/openticon/service/PackService.java
+++ backend/spring/src/main/java/io/ssafy/openticon/service/PackService.java
@@ -1,9 +1,75 @@
 package io.ssafy.openticon.service;
 
+import io.ssafy.openticon.dto.EmoticonPack;
+import io.ssafy.openticon.repository.PackRepository;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.reactive.function.client.WebClient;
+
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class PackService {
 
-    public void emoticonPackUpload()
+    private final WebClient webClient;
+    private final PackRepository packRepository;
+
+    public PackService(WebClient webClient, PackRepository packRepository){
+        this.webClient=webClient;
+        this.packRepository=packRepository;
+    }
+
+    public void emoticonPackUpload(EmoticonPack emoticonPack){
+        MultipartFile thumbnailImg= emoticonPack.getThumbnailImg();
+        MultipartFile listImg= emoticonPack.getListImg();
+
+        String thumbnailImgUrl=saveImage(thumbnailImg);
+        String listImgUrl=saveImage(listImg);
+
+        List<String> emoticonsUrls=new ArrayList<>();
+        for(MultipartFile emoticon: emoticonPack.getEmoticons()){
+            emoticonsUrls.add(saveImage(emoticon));
+        }
+    }
+
+    private String saveImage(MultipartFile image){
+        String uploadServerUrl="http://localhost:8070/upload/image";
+
+        File tempFile = null;
+        try {
+            tempFile = File.createTempFile("upload", image.getOriginalFilename());
+            image.transferTo(tempFile);
+
+            String imageUrl = webClient.post()
+                    .uri(uploadServerUrl)
+                    .contentType(MediaType.MULTIPART_FORM_DATA)
+                    .bodyValue(createMultipartBody(tempFile, image.getOriginalFilename()))
+                    .retrieve()
+                    .bodyToMono(String.class)
+                    .block();
+
+            return imageUrl;
+
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to save image",e);
+        }
+
+
+    }
+
+    private MultiValueMap<String, Object> createMultipartBody(File file, String fileName) {
+        MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
+        body.add("upload", new FileSystemResource(file));
+        return body;
+    }
 }
