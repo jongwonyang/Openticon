5be816df886369fbe502aa5e36a98157e54b29ec
양종원 <yjw963@gmail.com>
2024-10-29 16:49:54 +0900

    [android/search] Layout search menu !23

---
+++ android/app/src/main/java/io/ssafy/openticon/ui/sample/CarouselSample.kt
@@ -0,0 +1,58 @@
+package io.ssafy.openticon.ui.sample
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.carousel.HorizontalMultiBrowseCarousel
+import androidx.compose.material3.carousel.rememberCarouselState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+
+data class CarouselItem(
+    val id: Int,
+    @DrawableRes val imageResId: Int,
+    @StringRes val contentDescriptionResId: Int
+)
+
+val items = EmoticonPackSampleData.latestPacks
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview
+@Composable
+fun CarouselSample() {
+    HorizontalMultiBrowseCarousel(
+        state = rememberCarouselState { items.count() },
+        modifier = Modifier.width(412.dp).height(221.dp),
+        preferredItemWidth = 186.dp,
+        itemSpacing = 8.dp,
+        contentPadding = PaddingValues(horizontal = 16.dp),
+    ) { i ->
+        val item = items[i]
+
+        Column {
+            AsyncImage(
+                model = ImageRequest.Builder(context = LocalContext.current)
+                    .data(item.thumbnail)
+                    .crossfade(true)
+                    .build(),
+                contentDescription = item.title,
+                modifier = Modifier.height(205.dp).maskClip(MaterialTheme.shapes.extraLarge),
+                contentScale = ContentScale.Crop
+            )
+            Text(item.title)
+            Text(item.author)
+        }
+    }
+}
--- android/app/src/main/java/io/ssafy/openticon/ui/screen/StoreScreen.kt
+++ android/app/src/main/java/io/ssafy/openticon/ui/screen/StoreScreen.kt
@@ -1,25 +1,42 @@
 package io.ssafy.openticon.ui.screen
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import io.ssafy.openticon.ui.sample.CarouselSample
+import io.ssafy.openticon.ui.sample.EmoticonPackSampleData
 
+@Preview(showBackground = false)
 @Composable
 fun StoreScreen() {
     val scrollState = rememberScrollState()
+    val latestPacks = EmoticonPackSampleData.latestPacks
 
     Column(
         modifier = Modifier
             .fillMaxSize()
             .verticalScroll(scrollState)
     ) {
-        for (i in 1..50) {
-            Button(onClick = {}) { Text("Button") }
+        Row(
+            Modifier.padding(16.dp)
+        ) {
+            Text(
+                text = "신규",
+                style = MaterialTheme.typography.titleLarge,
+                fontWeight = FontWeight.Bold
+            )
         }
+
+        CarouselSample()
     }
 }
