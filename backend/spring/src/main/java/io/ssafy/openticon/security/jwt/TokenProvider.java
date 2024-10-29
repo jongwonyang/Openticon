a8428256ebc8e9c11956de8bf03d08ef384eb833
qwsa522 <qwsa522@naver.com>
2024-10-29 17:14:40 +0900

    [spring/security] 토큰 만료시간 1년으로 수정

--- backend/spring/src/main/java/io/ssafy/openticon/security/jwt/TokenProvider.java
+++ backend/spring/src/main/java/io/ssafy/openticon/security/jwt/TokenProvider.java
@@ -23,7 +23,7 @@ import java.util.Date;
 @Component
 public class TokenProvider {
 
-    private static final long ACCESS_TOKEN_EXPIRE_TIME_IN_MILLISECONDS = 1000 * 60 * 30; // 30min
+    private static final long ACCESS_TOKEN_EXPIRE_TIME_IN_MILLISECONDS = 1000L * 60 * 60 * 24 * 365;//1년
     @Value("${jwt.secret}")
     private String secret;
     private Key key;
