b1270b7533eb16f921bb194d415e1aed4feb390e
Chansu Park <1265pcs@gmail.com>
2024-10-30 09:52:06 +0900

    [frontend/vue] 메인 컴포넌트 이름 수정

---
+++ frontend/vue/src/views/MainView.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import MainBanner from '@/components/main/banner/Banner.vue';
+</script>
+
+<template>
+    <MainBanner />
+    <div class="container p-4 mx-auto max-w-screen-lg">
+        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+            <div class="border p-4">
+                하이1
+            </div>
+            <div class="border p-4">
+                하이2
+            </div>
+            <div class="border p-4">
+                하이3
+            </div>
+            <div class="border p-4">
+                하이4
+            </div>
+            <div class="border p-4">
+                하이5
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+</style>
