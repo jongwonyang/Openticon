a0bf002fdeb3829e5306e5f473d97bfe96662de9
Chansu Park <1265pcs@gmail.com>
2024-10-30 09:58:52 +0900

    [vue/mainpage] 메인페이지 캐러셀 추가

--- frontend/vue/src/components/common/navigation/NavBar.vue
+++ frontend/vue/src/components/common/navigation/NavBar.vue
@@ -1,35 +1,66 @@
 <!-- Start Generation Here -->
 <template>
-  <nav class="p-4 container mx-auto">
-    <ul class="flex space-x-4">
+  <div class="flex justify-between items-center px-2 sm:px-4 md:px-6 pt-3">
+    <div class="w-12">
+    </div>
+    <h1 class="text-2xl font-bold">
+      <span style="font-family: 'NanumSquareNeoHeavy', sans-serif;">OPEN</span><span
+        style="font-family: 'NanumSquareNeoLight', sans-serif;">TICON</span>
+    </h1>
+    <div>
+      <img src="" alt="프로필 사진" class="w-12 h-12 rounded-full" />
+    </div>
+  </div>
+  <nav class="mt-2 px-2 pt-4  mx-auto border-b-2 border-gray-200">
+    <ul class="flex justify-center">
       <li>
-        <RouterLink
-          to="/"
-          class="text-black hover:text-gray-300 font-bold text-2xl"
-          >홈</RouterLink
-        >
+        <RouterLink :to="{ name: 'main' }" :class="['nav-link', { active: isActive('main') }]">
+          메인
+        </RouterLink>
       </li>
       <li>
-        <RouterLink
-          to="/about"
-          class="text-black hover:text-gray-300 font-bold text-2xl"
-          >소개</RouterLink
-        >
+        <RouterLink :to="{ name: 'new' }" :class="['nav-link', { active: isActive('new') }]">
+          신규
+        </RouterLink>
       </li>
       <li>
-        <RouterLink
-          to="/debug"
-          class="text-black hover:text-gray-300 font-bold text-2xl"
-          >디버그</RouterLink
-        >
-      </li>   
+        <RouterLink :to="{ name: 'popular' }" :class="['nav-link', { active: isActive('popular') }]">
+          인기
+        </RouterLink>
+      </li>
+      <li>
+        <RouterLink :to="{ name: 'upload' }" :class="['nav-link', { active: isActive('upload') }]">
+          업로드
+        </RouterLink>
+      </li>
     </ul>
   </nav>
 </template>
 
 <script setup lang="ts">
-import { RouterLink } from "vue-router";
+import { RouterLink, useRoute } from "vue-router";
+
+const route = useRoute();
+
+function isActive(routeName: string) {
+  return route.name === routeName;
+}
 </script>
 
-<style scoped></style>
+<style scoped>
+.nav-link {
+  font-family: 'NanumSquareNeo', sans-serif;
+  
+  @apply text-black font-light text-lg;
+  @apply flex justify-center items-center px-8 sm:px-8 md:px-12 lg:px-16 py-2;
+  @apply hover:border-b-2 hover:border-black;
+  @apply hover:font-bold hover:bg-gray-100;
+
+  @apply cursor-pointer;
+}
+
+.nav-link.active {
+  @apply border-b-2 border-black font-bold; /* 현재 경로일 때 강조 효과 */
+}
+</style>
 <!-- End Generation Here -->
