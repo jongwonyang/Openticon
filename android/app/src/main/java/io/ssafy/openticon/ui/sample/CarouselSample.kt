791885ed77a3dc2480fa707a53165ce9e2508c25
김용원 <qwsa522@naver.com>
2024-10-30 14:19:08 +0900

    [Android/store] 스토어 페이지 ui 수정

--- android/app/src/main/java/io/ssafy/openticon/ui/sample/CarouselSample.kt
+++ android/app/src/main/java/io/ssafy/openticon/ui/sample/CarouselSample.kt
@@ -12,9 +12,12 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.carousel.HorizontalMultiBrowseCarousel
 import androidx.compose.material3.carousel.rememberCarouselState
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
@@ -34,7 +37,7 @@ val items = EmoticonPackSampleData.latestPacks
 fun CarouselSample() {
     HorizontalMultiBrowseCarousel(
         state = rememberCarouselState { items.count() },
-        modifier = Modifier.width(412.dp).height(221.dp),
+        modifier = Modifier.width(412.dp).height(230.dp),
         preferredItemWidth = 186.dp,
         itemSpacing = 8.dp,
         contentPadding = PaddingValues(horizontal = 16.dp),
@@ -48,11 +51,24 @@ fun CarouselSample() {
                     .crossfade(true)
                     .build(),
                 contentDescription = item.title,
-                modifier = Modifier.height(205.dp).maskClip(MaterialTheme.shapes.extraLarge),
+                modifier = Modifier.height(180.dp).maskClip(MaterialTheme.shapes.extraLarge),
                 contentScale = ContentScale.Crop
             )
-            Text(item.title)
-            Text(item.author)
+//            Column(
+//            ) {
+//                Text(
+//                    text = item.title,
+//                    fontWeight = FontWeight.Bold,
+//                    style = MaterialTheme.typography.bodyLarge
+//                )
+//                Text(
+//                    text = item.author,
+//                    color = Color.Gray, // 회색으로 설정
+//                    style = MaterialTheme.typography.bodySmall
+//                )
+//            }
+
+
         }
     }
 }
