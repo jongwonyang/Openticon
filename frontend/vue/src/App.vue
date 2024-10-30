1de425d2d0b944fc0abbfbe26930674dcc63b805
Chansu Park <1265pcs@gmail.com>
2024-10-30 12:51:59 +0900

    [frontend/vue] 푸터 추가

--- frontend/vue/src/App.vue
+++ frontend/vue/src/App.vue
@@ -2,12 +2,14 @@
 import { RouterLink, RouterView } from "vue-router";
 import AlertList from "./components/common/alert/AlertList.vue";
 import NavBar from "./components/common/navigation/NavBar.vue";
+import Footer from "./components/common/footer/Footer.vue";
 </script>
 
 <template>
   <AlertList />
   <NavBar />
   <RouterView />
+  <Footer />
 </template>
 
 <style scoped>
