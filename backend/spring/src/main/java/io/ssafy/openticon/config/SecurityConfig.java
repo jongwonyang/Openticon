3f9bca3f2092cf33ff5060f47718faa6464609bd
roista57 <roista57@gmail.com>
2024-10-31 14:01:20 +0900

    [spring/config] CORS 수정

--- backend/spring/src/main/java/io/ssafy/openticon/config/SecurityConfig.java
+++ backend/spring/src/main/java/io/ssafy/openticon/config/SecurityConfig.java
@@ -92,7 +92,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 //        configuration.setAllowedOrigins(List.of("*")); // Allow all origins, or specify specific origins
-        configuration.setAllowedOriginPatterns(Arrays.asList("http://localhost:3000", "http://127.0.0.1:3000", "http://127.0.0.1:5173", "http://localhost:5173", "http://127.0.0.1:8080"));
+        configuration.setAllowedOriginPatterns(Arrays.asList("http://localhost:3000", "http://127.0.0.1:3000", "http://127.0.0.1:5173", "http://localhost:5173", "http://127.0.0.1:8080", "https://apitest.openticon.store"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*")); // Allow all headers
         configuration.setAllowCredentials(true); // If you need credentials
