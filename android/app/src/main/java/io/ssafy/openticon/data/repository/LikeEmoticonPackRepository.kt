d46f8e094f8e6dfc32b5e3f849a4b4dd179a8665
박병준 <h0568@naver.com>
2024-11-01 12:24:25 +0900

    [android/floating] Modify imageview to coil

--- android/app/src/main/java/io/ssafy/openticon/data/repository/LikeEmoticonPackRepository.kt
+++ android/app/src/main/java/io/ssafy/openticon/data/repository/LikeEmoticonPackRepository.kt
@@ -9,6 +9,7 @@ class LikeEmoticonPackRepository {
         return EmoticonPack("즐겨찾기", android.R.drawable.ic_menu_preferences, listOf(
                 Emoticon(R.drawable.icon_9, ""),
                 Emoticon(R.drawable.icon_28, ""),
+            Emoticon(R.drawable._2ac25c78c76acdfffc908990700d7abf43ad912ad8dd55b04db6a64cddaf76d, ""),
             )
         )
     }
