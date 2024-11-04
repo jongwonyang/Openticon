8ea96300904a7cd2fd3a477daa47e302f21c27a0
Chansu Park <1265pcs@gmail.com>
2024-11-04 16:36:40 +0900

    [vue/login] 로그인 처리 팝업 라우팅, 마이페이지 라우팅 추가

--- frontend/vue/src/router/index.ts
+++ frontend/vue/src/router/index.ts
@@ -6,6 +6,10 @@ import NewEmoticonListView from '@/views/NewEmoticonListView.vue'
 import PopularEmoticonListView from '@/views/PopularEmoticonListView.vue'
 import UploadEmoticonView from '@/views/UploadEmoticonView.vue'
 import EmoticonDetailView from '@/views/EmoticonDetailView.vue'
+import ProcessLoginView from '@/views/ProcessLogin.vue'
+import MypageView from '@/views/MypageView.vue'
+import { useUserStore } from '@/stores/user'
+import { makeWarningAlert } from '@/util/alert'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -34,6 +38,25 @@ const router = createRouter({
       path: '/packDetail/:id',
       name: 'packDetail',
       component: EmoticonDetailView
+    },
+    {
+      path: '/processLogin',
+      name: 'processLogin',
+      component: ProcessLoginView
+    },
+    {
+      path: '/mypage',
+      name: 'mypage',
+      component: MypageView,
+      beforeEnter: (to, from, next) => {
+        const isLoggedIn = useUserStore().isLogined; // 로그인 상태 확인
+        if (isLoggedIn) {
+          next(); // 로그인 상태이면 접근 허용
+        } else {
+          makeWarningAlert('로그인이 필요한 서비스입니다. 메인페이지로 이동합니다.');
+          next({ name: 'main' }); // 로그인 상태가 아니면 메인 페이지로 리다이렉트
+        }
+      },
     }
   ]
 })
---
+++ frontend/vue/src/views/ProcessLogin.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>ProcessLogin</h1>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted } from 'vue';
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+
+onMounted(() => {
+    console.log(route.query.access_token);
+    window.opener.postMessage(route.query.access_token , 'http://localhost:3000');
+    window.close();
+});
+</script> 
