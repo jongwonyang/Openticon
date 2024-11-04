b137e3f3399209b0afdba3a391d6295a6e41c93a
Chansu Park <1265pcs@gmail.com>
2024-11-04 16:36:10 +0900

    [vue/login] 사용자 스토어 추가

--- frontend/vue/src/stores/user.ts
+++ frontend/vue/src/stores/user.ts
@@ -1,7 +1,50 @@
-
+import apiClient from '@/util/apiClient';
 import { defineStore } from 'pinia'
+import { ref } from 'vue';
+
+ // Start of Selection
+interface UserInfo {
+    id: number;
+    email: string;
+    nickname: string;
+    profile_image: string;
+    createdAt: number;
+    updatedAt: number;
+    isResigned: boolean;
+    manager: boolean;
+    mobile_fcm: string;
+    web_fcm: string;
+    point: number;
+}
 
 export const useUserStore = defineStore('user', () => {
+    const accessToken = ref<string | null>(localStorage.getItem('accessToken') || null);
+    const isLogined = ref<boolean>(!!accessToken.value);
+    const userInfo = ref<UserInfo | null>(null);
 
-    return {}
+    const login = (token: string) => {
+        localStorage.setItem('accessToken', token);
+        apiClient.get('/member').then((res) => {
+            console.log(res.data);
+            userInfo.value = res.data;
+        });
+        accessToken.value = token;
+        isLogined.value = true;
+    }
+
+    if (accessToken.value) {
+        login(accessToken.value);
+    }
+
+    const logout = () => {
+        localStorage.removeItem('accessToken');
+        accessToken.value = null;
+        isLogined.value = false;
+        userInfo.value = null;
+    }
+
+    const getAccessToken = () => {
+        return accessToken.value;
+    }
+    return { isLogined, userInfo, getAccessToken, login, logout }
 })
