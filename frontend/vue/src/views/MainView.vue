401d62654cfe14edea349c87bbbb21359d95988f
Chansu Park <1265pcs@gmail.com>
2024-11-05 17:49:16 +0900

    [vue/mainpage] 메인페이지 개선

--- frontend/vue/src/views/MainView.vue
+++ frontend/vue/src/views/MainView.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts">
 import MainBanner from "@/components/main/banner/Banner.vue";
 import NewListShort from "@/components/main/newListShort/NewListShort.vue";
+import PopularListShort from "@/components/main/popularListShort/PopularListShort.vue";
+import SearchBar from "@/components/main/search/SearchBar.vue";
 </script>
 
 <template>
   <MainBanner />
+  <SearchBar />
   <NewListShort />
+  <PopularListShort />
   <div class="container p-4 mx-auto max-w-screen-lg">
     <div
       class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4"
