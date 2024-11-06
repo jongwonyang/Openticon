dc9de95aa3506935a583a7b8a5ddf0b55dabf260
박병준 <h0568@naver.com>
2024-11-06 10:16:37 +0900

    [android/DB] Modify likeEmoticon entity

--- android/app/src/main/java/io/ssafy/openticon/data/model/LikeEmoticon.kt
+++ android/app/src/main/java/io/ssafy/openticon/data/model/LikeEmoticon.kt
@@ -8,5 +8,7 @@ import kotlinx.serialization.Serializable;
 @Entity(tableName = "like_emoticons")
 data class LikeEmoticon(
     @PrimaryKey(autoGenerate = true) val id: Int = 0, // id는 자동 증가됨
-    val filePath: String
+    val filePath: String,
+    val title: String,
+    val packId: Int
 )
