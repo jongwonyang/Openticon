49c35ffb70606365b87ae15ef220c821647075c8
양종원 <yjw963@gmail.com>
2024-11-11 14:15:39 +0900

    [android/store] Design store header

--- android/app/src/main/java/io/ssafy/openticon/ui/screen/MyEmoticonsScreen.kt
+++ android/app/src/main/java/io/ssafy/openticon/ui/screen/MyEmoticonsScreen.kt
@@ -10,12 +10,12 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
@@ -23,8 +23,6 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.DragHandle
 import androidx.compose.material.icons.filled.VisibilityOff
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Divider
 import androidx.compose.material3.FilledIconButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -32,14 +30,14 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -47,7 +45,6 @@ import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.PointerInputChange
 import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.IntOffset
@@ -57,7 +54,6 @@ import androidx.compose.ui.zIndex
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import coil.compose.rememberAsyncImagePainter
-import coil.compose.rememberImagePainter
 import io.ssafy.openticon.data.model.EmoticonPackEntity
 import io.ssafy.openticon.ui.viewmodel.MyEmoticonViewModel
 import kotlin.math.roundToInt
@@ -125,19 +121,19 @@ fun MyEmoticonsScreen(
 
     Column(
         modifier = Modifier
-            .padding(16.dp)
+            .padding(horizontal = 16.dp)
     ) {
         // 헤더
         Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(64.dp),
             verticalAlignment = Alignment.CenterVertically
         ) {
             Text(
                 text = "내 이모티콘 관리",
                 style = MaterialTheme.typography.titleLarge,
-                fontWeight = FontWeight.Bold,
-                modifier = Modifier.padding(start = 16.dp)
+                fontWeight = FontWeight.Bold
             )
         }
 
@@ -149,7 +145,7 @@ fun MyEmoticonsScreen(
                 .fillMaxWidth(),
             horizontalArrangement = Arrangement.SpaceEvenly
         ) {
-            if(isVisible){
+            if (isVisible) {
                 FilledIconButton(
                     onClick = { isVisible = true },
                     modifier = Modifier
@@ -166,8 +162,7 @@ fun MyEmoticonsScreen(
                         Spacer(Modifier.height(4.dp))
                     }
                 }
-            }
-            else{
+            } else {
                 TextButton(
                     onClick = { isVisible = true },
                     modifier = Modifier
@@ -185,7 +180,7 @@ fun MyEmoticonsScreen(
                     }
                 }
             }
-            if(isVisible){
+            if (isVisible) {
                 TextButton(
                     onClick = { isVisible = false },
                     modifier = Modifier
@@ -202,8 +197,7 @@ fun MyEmoticonsScreen(
                         Spacer(Modifier.height(4.dp))
                     }
                 }
-            }
-            else{
+            } else {
                 FilledIconButton(
                     onClick = { isVisible = false },
                     modifier = Modifier
@@ -240,7 +234,12 @@ fun MyEmoticonsScreen(
                     onDragCancel = onDragCancel,
                     modifier = Modifier
                         .zIndex(if (isDragging) 1f else 0f)
-                        .offset { IntOffset(x = 0, y = if (isDragging) offsetY.roundToInt() else 0) }
+                        .offset {
+                            IntOffset(
+                                x = 0,
+                                y = if (isDragging) offsetY.roundToInt() else 0
+                            )
+                        }
                         .combinedClickable(
                             onClick = { navController.navigate("emoticonPack/${emoticonPack.uuid}") },
                             onLongClick = { onDragStart(index) }
@@ -293,7 +292,9 @@ fun EmoticonItem(
                         .width(50.dp)
                         .height(22.dp)
                         .background(
-                            color = if (sampleEmoticonPack.isPublic) Color(0xFFF4E845) else Color(0xFFD3D3D3),
+                            color = if (sampleEmoticonPack.isPublic) Color(0xFFF4E845) else Color(
+                                0xFFD3D3D3
+                            ),
                             shape = RoundedCornerShape(9.dp),
                         ),
                     contentAlignment = Alignment.Center
